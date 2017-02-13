9af3a71a4e99b90ca2a0d10e47e4284dbe9e69bd
SBKubric <sbkubric@gmail.com>
2017-02-13 22:24:06 +0300

    Added models.py

--- cinema_advisor.py
+++ cinema_advisor.py
@@ -1,17 +1,35 @@
-import requests
-import urllib.parse
+from models import Movie, List, Genre, Keyword, Base,
+from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine
+from sqlalchemy.sql import select
+from sqlalchemy.orm.session import sessionmaker
+import json
 from pprint import pprint
+from sqlite3 import Connection, DatabaseError
+
+import random
+import requests
+from tqdm import tqdm
+
+from models import Movie
 
 API_KEY_V3 = 'ea49129df62d63a56dc7324f7186f7fc'
+PROXY_API_URL = 'http://www.freeproxy-list.ru/api/proxy'
+DB_NAME = 'movies.db'
+AMOUNT_OF_MOVIES = 10
+
+Session = sessionmaker(autoflush=False)
 
 
-def load_json_data_from_url(base_url, url_params):
-    url = '{}?{}'.format(base_url, urllib.parse.urlencode(url_params))
-    response = requests.get(url)
-    return response.json()
+def fetch_proxy_list() -> list:
+    params = {'anonymity': 'false',
+              'token': 'demo'}
+    response = requests.get(PROXY_API_URL, params=params).text
+    proxies = response.split('\n')
+    return proxies
 
 
-def make_tmdb_api_request(method, api_key, extra_params=None):
+def make_tmdb_api_request(method, api_key, proxy_list, *, extra_params=None) -> dict:
     extra_params = extra_params or {}
     url = 'https://api.themoviedb.org/3{}'.format(method)
     params = {
@@ -19,35 +37,100 @@ def make_tmdb_api_request(method, api_key, extra_params=None):
         'language': 'ru',
     }
     params.update(extra_params)
-    return load_json_data_from_url(url, params)
+    proxy = {"http": random.choice(proxy_list)}
+    response = requests.get(url, params=params, proxies=proxy)
+    return response.json()
 
 
-def get_local_instance_db():
+def get_engine_and_initialize_db(Base) -> Engine:
+    engine = create_engine('sqlite:///movies.db')
+    Base.metadata.create_all(engine)
+    return engine
+
+
+def is_incomplete():
+    sess = Session()
+    movies = sess.query(Movie).order_by(Movie.movie_id)
+    return len(movies) < AMOUNT_OF_MOVIES
+
+
+def persist_data_to_db(movie_object, db_connection) -> bool:
     pass
 
-
-def create_db():
-    pass
-
-
-def json_to_db_table(json_movie):
-    pass
+def create_random_id_tmdb_list():
+    return random.sample(range(2, 500000), k=10000)
 
 
-def table_to_json(table_movie_id, db):
-    pass
+def create_movie_json(id_tmdb):
+    proxy_list = fetch_proxy_list()
+    movie_json = make_tmdb_api_request(
+        method='/movie/{}'.format(id_tmdb),
+        api_key=API_KEY_V3,
+        proxy_list=proxy_list
+    )
+    keywords = make_tmdb_api_request(
+        method='/movies/{}/keywords'.format(id_tmdb),
+        api_key=API_KEY_V3,
+        proxy_list=proxy_list
+    )
+    movie_json.update(keywords)
+    lists = make_tmdb_api_request(
+        method='/movies/{}/lists'.format(id_tmdb),
+        api_key=API_KEY_V3,
+        proxy_list=proxy_list
+    )
+    movie_json.update(lists)
+    alternative_titles = make_tmdb_api_request(
+        method='/movies/{}/alternative_titles'.format(id_tmdb),
+        api_key=API_KEY_V3, proxy_list=proxy_list
+    )
+    movie_json.update(alternative_titles)
+    if 'id' in movie_json.keys():
+        return movie_json
+    return None
 
 
-def get_similar_titles_ids_list(movie_title, db):
-    pass
+def fetch_db(Base, engine):
+    sess = Session()
+    movies = sess.query(Movie).order_by(Movie.movie_id)
+    ids_tmdb = create_random_id_tmdb_list()
+    index = 0
+    for _ in tqdm(range(AMOUNT_OF_MOVIES), total=AMOUNT_OF_MOVIES - len(movies), unit='movie'):
+        while True:
+            json_movie = create_movie_json(ids_tmdb[index])
+            index += 1
+            if json_movie:
+                persist_data_to_db(json_movie)
+                break
+    return db_connection
 
 
-def find_id_by_title(movie_title):
-    pass
+def fetch_movie_values_frm_db(table_movie_id, db_connection) -> dict:
+    cursor = db_connection.cursor()
+    cursor.execute('SELECT * FROM movie WHERE movie.id=?', (table_movie_id,))
+    return  cursor.fetchone()
 
 
-def create_json_frm_db_entry(pivot_id):
-    pass
+
+def get_similar_titles_ids_list(movie_title, db_connection) -> list:
+    cursor = db_connection.cursor()
+    cursor.execute(
+        'SELECT movie_id FROM titles WHERE movie_title LIKE ?;', ('%{}%'.format(movie_title),)
+    )
+    return cursor.fetchall()
+
+
+def fetch_movie_frm_db(pivot_id, db_connection) -> Movie:
+    values = fetch_movie_values_frm_db(pivot_id, db_connection)
+    movie_model = Movie()
+    movie_model.movie_id, = values[0]
+    movie_model.title_id = values[1]
+
+
+
+
+
+
 
 
 def create_json_list_frm_db_entries(similar_titles_ids_list):
@@ -58,34 +141,36 @@ def get_recommendations_list_frm_db(pivot_json, movies_json_list):
     pass
 
 
+
+
 if __name__ == '__main__':
-    # json_300_spartan_details = make_tmdb_api_request(method='/movie/1271', api_key=API_KEY_V3)
+    # proxy_list = fetch_proxy_list()
+    # json_300_spartan_details = make_tmdb_api_request(method='/movie/69892', api_key=API_KEY_V3, proxy_list=proxy_list)
     # pprint(json_300_spartan_details)
     # print('\n\n')
-    # json_300_spartan_keywords = make_tmdb_api_request(method='/movie/1271/keywords', api_key=API_KEY_V3)
-    # pprint(json_300_spartan_keywords)
-    # print('\n\n')
-    # json_300_spartan_alternative_titles = make_tmdb_api_request(method='/movie/1271/alternative_titles', api_key=API_KEY_V3)
-    # pprint(json_300_spartan_alternative_titles)
-    # print('\n\n')
-    # json_300_spartan_user_lists = make_tmdb_api_request(method='/movie/1271/lists', api_key=API_KEY_V3)
-    # pprint(json_300_spartan_user_lists)
+    # json_300_spartan_keywords = make_tmdb_api_request(method='/movie/69892/keywords', api_key=API_KEY_V3, proxy_list=proxy_list)
+    # json_300_spartan_details.update(json_300_spartan_keywords)
+    # json_300_spartan_alternative_titles = make_tmdb_api_request(method='/movie/69892/alternative_titles', api_key=API_KEY_V3, proxy_list=proxy_list)
+    # json_300_spartan_details.update(json_300_spartan_alternative_titles)
+    # json_300_spartan_user_lists = make_tmdb_api_request(method='/movie/69892/lists', api_key=API_KEY_V3, proxy_list=proxy_list)
+    # json_300_spartan_details.update(json_300_spartan_user_lists)
+    # pprint(json_300_spartan_details)
+
     print("Welcome to SBKubric's Cinema Advisor!")
-    print('Checking the film database...')
-    db = get_local_instance_db()
-    if not db:
-        print('Creating database...')
-        db = create_db()
-        print('OK')
-    else:
-        print('OK')
-    movie_title = input('Please, enter the name of the movie that would be a pivot: ')
-    print('Searching for similar titles...')
-    similar_titles_ids_list = get_similar_titles_ids_list(movie_title, db)
-    print('Fetching the data of the pivot movie...')
-    pivot_id = find_id_by_title(movie_title)
-    pivot_json = create_json_frm_db_entry(pivot_id)
-    print('Fetching the data of similar titles...')
-    movies_json_list = create_json_list_frm_db_entries(similar_titles_ids_list)
-    print('Building a recommendations list...')
-    recommendations_list = get_recommendations_list_frm_db(pivot_json, movies_json_list)
+    engine = get_engine_and_initialize_db(Base)
+    Session.configure(bind=engine)
+    if is_incomplete():
+        print('Fetching the database...')
+        fetch_db()
+    print('OK')
+    # movie_title = input('Please, enter the name of the movie that would be a pivot: ')
+    # print('Searching for similar titles...')
+    # similar_titles_ids_list = get_similar_titles_ids_list(movie_title, db_connection)
+    # print('Fetching the data of similar titles...')
+    # movies_json_list = create_json_list_frm_db_entries(similar_titles_ids_list)
+    # print('Building a recommendations list...')
+    # recommendations_list = get_recommendations_list_frm_db(pivot_json, movies_json_list)
+    # db_connection.close()
+    # print('The recommended films:')
+    # pprint(recommendations_list)
+    # print(similar_titles_ids_list)
---
+++ models.py
@@ -0,0 +1,119 @@
+from sqlalchemy import Column, Integer, String, REAL, ForeignKey, Table, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+movie_genres = Table('movie_genres', Base.metadata,
+                     Column('genre_id', ForeignKey('genres.genre_id'), primary_key=True),
+                     Column('movie_id', ForeignKey('movies.movie_id'), primary_key=True)
+)
+movie_lists = Table('movie_lists', Base.metadata,
+                     Column('list_id', ForeignKey('lists.list_id'), primary_key=True),
+                     Column('movie_id', ForeignKey('movies.movie_id'), primary_key=True)
+)
+movie_keywords = Table('movie_keywords', Base.metadata,
+                       Column('keyword_id', ForeignKey('keywords.keyword_id'), primary_key=True),
+                       Column('movie_id', ForeignKey('movies.movie_id'), primary_key=True)
+)
+
+
+class Movie (Base):
+    __tablename__ = 'movies'
+
+    movie_id = Column(Integer, primary_key=True)
+    vote_average = Column(REAL)
+    title_id = Column(Integer, ForeignKey('titles.title_id'))
+
+    titles = relationship('Title', back_populates='movies')
+
+    genres = relationship('Genre',
+                          secondary=movie_genres,
+                          back_populates='movies')
+    keywords = relationship('Keyword',
+                            secondary=movie_keywords,
+                            back_populates='movies')
+    lists = relationship('List',
+                         secondary=movie_lists,
+                         back_populates='movies')
+
+    def __init__(self, movie_id, vote_average):
+        self.movie_id = movie_id
+        self.vote_average = vote_average
+
+    def __repr__(self):
+        return "<Movie(movie_id={}, title_id={}, vote_average={})>".format(
+            self.movie_id, self.title_id, self.vote_average
+        )
+
+
+class Title (Base):
+    __tablename__ = 'titles'
+
+    title_id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String)
+    movie_id = Column(Integer, ForeignKey('movies.movie_id'))
+
+    movie = relationship('Movie', back_populates='titles')
+
+    def __init__(self, title, movie_id):
+        self.title = title
+        self.movie_id = movie_id
+
+    def __repr__(self):
+        return "<Title(title_id={}, title='{}', movie_id={})>".format(
+            self.title_id, self.title, self.movie_id
+        )
+
+
+class Keyword (Base):
+    __tablename__ = 'keywords'
+
+    keyword_id = Column(Integer, primary_key=True)
+    keyword = Column(String)
+
+    movies = relationship('Movie', secondary=movie_keywords, back_populates='keywords')
+
+    def __init__(self, keyword_id, keyword):
+        self.keyword_id = keyword_id
+        self.keyword = keyword
+
+    def __repr__(self):
+        return "<Keyword(keyword_id={}, keyword='{}', movie_id={})>".format(
+            self.keyword_id, self.keyword, self.movie_id
+        )
+
+
+class Genre (Base):
+    __tablename__ = 'genres'
+
+    genre_id = Column(Integer, primary_key=True)
+    genre = Column(String)
+
+    movies = relationship('Movie', secondary=movie_genres, back_populates='genres')
+
+    def __init__(self, genre_id, genre):
+        self.genre_id = genre_id
+        self.genre = genre
+
+    def __repr__(self):
+        return "<Genre(genre_id={}, genre='{}')>".format(
+            self.genre_id, self.genre
+        )
+
+
+class List (Base):
+    __tablename__ = 'lists'
+
+    list_id = Column(Integer, primary_key=True)
+    list_title = Column(String)
+
+    movies = relationship('Movie', secondary=movie_lists, back_populates='lists')
+
+    def __init__(self, list_id, list_title):
+        self.list_id = list_id
+        self.list_title = list_title
+
+    def __repr__(self):
+        return "<List(id={}, list_title='{}')>".format(
+            self.list_id, self.list_title
+        )
